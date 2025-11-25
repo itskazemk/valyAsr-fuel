05002110171326ffd0a0afbd80de0b2ba410af7a
Kazem <itskazemk@gmail.com>
2025-11-25 22:30:29 +0330

    fuel price remote function

--- src/routes/base-info/fuel-price/fuelPrice.remote.ts
+++ src/routes/base-info/fuel-price/fuelPrice.remote.ts
@@ -1,7 +1,24 @@
-import { query } from '$app/server';
+import { command, query } from '$app/server';
 import { db } from '$lib/server/db';
 import { fuelPrices } from '$lib/server/db/schema';
+import * as v from 'valibot';
 
 export const getFuelPrices = query(async () => {
 	return await db.select().from(fuelPrices);
 });
+
+export const createFuelPrice = command(
+	v.object({ startDate: v.string(), endDate: v.string(), type: v.number(), amount: v.number() }),
+	async () => {},
+);
+
+export const updateFuelPrice = command(
+	v.object({
+		id: v.pipe(v.string(), v.uuid()),
+		startDate: v.string(),
+		endDate: v.string(),
+		type: v.number(),
+		amount: v.number(),
+	}),
+	async () => {},
+);
