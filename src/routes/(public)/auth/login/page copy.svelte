790ad03398fae9c080be346aec0a51d5991d4f1e
Kazem <itskazemk@gmail.com>
2025-12-11 20:48:20 +0330

    login page

--- src/routes/(public)/auth/login/page copy.svelte
+++ src/routes/(public)/auth/login/page copy.svelte
@@ -5,7 +5,7 @@
 	let { form }: { form: ActionData } = $props();
 </script>
 
-<h1>Login</h1>
+<h1>Login/Register</h1>
 <form method="post" action="?/login" use:enhance class="grid grid-cols-3 gap-2">
 	<label>
 		Username
@@ -14,7 +14,27 @@
 			class="mt-1 rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
 		/>
 	</label>
-
+	<label>
+		firstName
+		<input
+			name="firstName"
+			class="mt-1 rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+		/>
+	</label>
+	<label>
+		lastName
+		<input
+			name="lastName"
+			class="mt-1 rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+		/>
+	</label>
+	<label>
+		role
+		<input
+			name="role"
+			class="mt-1 rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+		/>
+	</label>
 	<label>
 		Password
 		<input
@@ -25,6 +45,11 @@
 	</label>
 	<button class="rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
 		>Login</button
+	>
+	<button
+		formaction="?/register"
+		class="rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
+		>Register</button
 	>
 </form>
 <p style="color: red">{form?.message ?? ''}</p>
