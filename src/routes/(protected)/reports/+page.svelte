92ed1968e1e78a351f2dc21fc6ff6f2f81c51fdf
Kazem <itskazemk@gmail.com>
2026-01-26 21:34:18 +0330

    test

--- src/routes/(protected)/reports/+page.svelte
+++ src/routes/(protected)/reports/+page.svelte
@@ -1,13 +1,25 @@
 <div class="flex h-full items-center justify-center gap-2">
-	<div class="animate-bounce border border-dashed border-black p-2">
+	<!-- <div class="animate-bounce border border-dashed border-black p-2">
 		<span>در حال تکمیل ...</span>
-	</div>
-	<!-- <div class="border-8 border-black px-2">
+	</div> -->
+	<div class="border-8 border-black px-2">
 		<div>کاظم کمالی اصل</div>
 		<div class="place-self-center">114 1 428</div>
 	</div>
 	<div class="border-8 border-black bg-red-500 px-2">
 		<div>کاظم کمالی اصل</div>
 		<div class="place-self-center">114 1 428</div>
-	</div> -->
+	</div>
+
+	<div class="grid h-16 w-48 grid-cols-4 border-8 border-black">
+		<div class="bg-red-500"></div>
+		<div></div>
+		<div></div>
+		<div class="bg-red-500"></div>
+
+		<div class="absolute h-14 w-44 text-lg font-bold">
+			<div class="font-bnazanin place-self-center">محمدکاظم کمالی‌اردکانی</div>
+			<div class="font-bnazanin place-self-center">114 1 428</div>
+		</div>
+	</div>
 </div>
