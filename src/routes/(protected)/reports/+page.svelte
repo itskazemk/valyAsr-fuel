e4dab1e68cb556e0ba5bda24f3d05445dd5b89ae
Kazem <itskazemk@gmail.com>
2026-01-28 21:20:22 +0330

    soldier code

--- src/routes/(protected)/reports/+page.svelte
+++ src/routes/(protected)/reports/+page.svelte
@@ -2,24 +2,68 @@
 	<!-- <div class="animate-bounce border border-dashed border-black p-2">
 		<span>در حال تکمیل ...</span>
 	</div> -->
-	<div class="border-8 border-black px-2">
-		<div>کاظم کمالی اصل</div>
-		<div class="place-self-center">114 1 428</div>
-	</div>
-	<div class="border-8 border-black bg-red-500 px-2">
-		<div>کاظم کمالی اصل</div>
-		<div class="place-self-center">114 1 428</div>
-	</div>
 
-	<div class="grid h-16 w-48 grid-cols-4 border-8 border-black">
-		<div class="bg-red-500"></div>
-		<div></div>
-		<div></div>
-		<div class="bg-red-500"></div>
-
-		<div class="absolute h-14 w-44 text-lg font-bold">
-			<div class="font-bnazanin place-self-center">محمدکاظم کمالی‌اردکانی</div>
-			<div class="font-bnazanin place-self-center">114 1 428</div>
+	<!-- <div class="card">
+		<div class="content">
+			<h2 class="font-bnazanin text-2xl font-bold">محمدکاظم کمالی‌اردکانی</h2>
+			<div class="numbers">
+				<span>428</span>
+				<span>2</span>
+				<span>114</span>
+			</div>
 		</div>
-	</div>
+	</div> -->
 </div>
+
+<style>
+	.card {
+		position: relative;
+		width: 300px;
+		height: 80px;
+		overflow: hidden;
+		border: 10px solid black;
+	}
+
+	/* Left triangle */
+	.card::before {
+		content: '';
+		position: absolute;
+		left: 0;
+		top: 0;
+		border-top: 60px solid red;
+		border-right: 100px solid transparent;
+		z-index: 1;
+	}
+
+	/* Right triangle */
+	.card::after {
+		content: '';
+		position: absolute;
+		right: 0;
+		top: 0;
+		border-bottom: 60px solid red;
+		border-left: 100px solid transparent;
+		z-index: 1;
+	}
+
+	/* 👇 content above triangles */
+	.content {
+		position: relative;
+		z-index: 2;
+		height: 100%;
+		padding: 0 24px;
+		text-align: center;
+	}
+
+	.numbers {
+		display: flex;
+		justify-content: center;
+		gap: 48px;
+	}
+
+	.numbers span {
+		font-family: 'B Nazanin';
+		font-size: 24px;
+		font-weight: bold;
+	}
+</style>
