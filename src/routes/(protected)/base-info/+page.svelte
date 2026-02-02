631e2262fd43f6358b9ee5a13c6c294b77bfc194
Kazem <itskazemk@gmail.com>
2026-02-02 17:45:12 +0330

    fix: using $inspect after remoteFunction causes server error in build so I commented it

--- src/routes/(protected)/base-info/+page.svelte
+++ src/routes/(protected)/base-info/+page.svelte
@@ -3,13 +3,13 @@
 
 	let values = await getBaseInfo(null);
 
-	$inspect(111, values);
+	console.log(111, values);
 </script>
 
 <div>
 	{#each values as value}
-		<a class="rounded-md p-2 hover:bg-blue-300" href="/base-info/{value.id}"
-			><p class="inline">{value.persianTitle}</p></a
+		<a class="rounded-md p-2 hover:bg-blue-300" href="/base-info/{value.id}">
+			<p class="inline">{value.persianTitle}</p></a
 		>
 	{/each}
 	<a class="rounded-md p-2 hover:bg-blue-300" href="/base-info/fuel-price">قیمت سوخت</a>
